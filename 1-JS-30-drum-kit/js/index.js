ded978ebac439c713b499b08d20ecd50bddabbf1
Szymon <jezyk2796@op.pl>
2021-06-09 17:09:08 +0200

    code cleanup. Playing audio improved

--- 1-JS-30-drum-kit/js/index.js
+++ 1-JS-30-drum-kit/js/index.js
@@ -1,16 +1,18 @@
-const divs = document.querySelectorAll('div[data-key]');
-const arrKeys = ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l']
+const arrKeys = ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'];
 
 window.addEventListener('keydown', e => {
   arrKeys.forEach((el) => {
     if (e.key === el) {
-      const div = document.querySelector(`div[data-key="${e.key}"]`)
+      const div = document.querySelector(`div[data-key="${e.key}"]`);
 
       div.classList.add('playing');
       setTimeout(() => {
         div.classList.remove('playing');
       }, 70)
-      document.querySelector(`audio[data-key="${e.key}"]`).play()
+
+      const audio = document.querySelector(`audio[data-key="${e.key}"]`);
+      audio.currentTime = 0;
+      audio.play();
     }
   })
 })
